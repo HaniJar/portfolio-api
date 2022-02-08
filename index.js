ec2b7b350f002c3c7f6b9cc0e6cebea3211c313d
Haniah Jardien <jardienhaniah@gmail.com>
2022-02-08 14:45:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const projectRoutes = require("./routes/projectRoutes");
+const testimonialRoutes = require("./routes/testimonialRoutes");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ msg: "welcome to haniah's backend" });
+});
+
+app.use("/projects", projectRoutes);
+app.use("/testimonials", testimonialRoutes);
+app.listen(5000);
